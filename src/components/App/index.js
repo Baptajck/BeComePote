847cfb0649b6a3c535a5add42d3b00b0284a3ea2
Baptajck <parville.baptiste@gmail.com>
2020-02-10 09:47:10 +0100

    React init
    Project setup React
    
    Co-Authored-By: TaLoche <taloche@users.noreply.github.com>

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+import './app.scss';
+
+// == Composant
+const App = () => (
+  <div id="app">
+    App React
+  </div>
+);
+
+// == Export
+export default App;
