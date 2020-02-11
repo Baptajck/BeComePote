507695fed4a7d5ec7cfd001bc4d487a89d0ef1b9
Baptajck <parville.baptiste@gmail.com>
2020-02-11 09:34:52 +0100

    Sign up setup
    Branch creation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,10 +4,12 @@ import React from 'react';
 // == Import : local
 import './app.scss';
 import HomePage from 'src/components/HomePage';
+import SignUp from 'src/components/SignUp';
 
 // == Composant
 const App = () => (
   <div>
+    <SignUp />
     <HomePage />
   </div>
 );
---
+++ src/components/SignUp/index.js
@@ -0,0 +1,14 @@
+// == Import NPM
+import React from 'react';
+
+// == Import Local
+import './signup.scss';
+
+const SignUp = () => (
+  <div>
+    <h1>Test</h1>
+  </div>
+);
+
+// == Export
+export default SignUp;
