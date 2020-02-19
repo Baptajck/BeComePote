a5495f19a4ef2473f612943b1eb8dfeb85b6a9fe
Baptajck <parville.baptiste@gmail.com>
2020-02-19 16:56:45 +0100

    amelioration create account with "objection-plugin"
    Avec objection plugin j'ai pu mettre dans le model ce que je ne voulais pas qui est en double dans la BBD ici l'email

--- backend/Models/Users.js
+++ backend/Models/Users.js
@@ -1,9 +1,16 @@
 const { Model } = require('objection');
+// Import the plugin.
+const unique = require('objection-unique')({
+  fields: ['email'],
+  identifiers: ['id'],
+});
+
 const knex = require('../connexion');
 
 Model.knex(knex);
 
-class User extends Model {
+
+class User extends unique(Model) {
   static get tableName() {
     return 'users';
   }
